282439bd74a8de2fe0997dce069c53c0acefa5f1
KL33M1NG <oscardanielbarrionuevo@gmail.com>
2026-05-23 11:32:59 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxON0o9XLdUWMr8bQPy40hhJbV3NDL7chuwYQVomEAZWxI9wJZ7Lkq_W35haq2huUWccg/exec';
+const API_TOKEN = '226452K@rko932566258233karko';
